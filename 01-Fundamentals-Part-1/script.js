23e6b755cda149cdf78a027ae344118bbe8aa568
ROCabral <rocabral.adm@gmail.com>
2024-07-15 20:59:19 -0300

    aprendendo data types em javascript.

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,6 +1,23 @@
-let js = "amazing";
-if (js === "amazing") {
-  alert("JavaScript is fun!");
-}
-
+//let js = "amazing";
 console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Matilda";
+console.log("print firstName = " + firstName);
+
+let javaScriptIsFun = true;
+console.log("Is javaScript fun? " + javaScriptIsFun);
+
+let num = 55.95;
+console.log("print num = " + num);
+
+console.log("print typeof firstName = " + typeof firstName);
+console.log("print typeof javaScriptIsFun = " + typeof javaScriptIsFun);
+console.log("print typeof num = " + typeof num);
+
+//variable with reasigned value
+javaScriptIsFun = "yes";
+console.log("print javascriptIsFun variable now = " + javaScriptIsFun);
+console.log("print typeof javaScriptIsFun = " + typeof javaScriptIsFun);
