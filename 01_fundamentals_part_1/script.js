c480b35e6692485ad8b614c52ead9b9e52b36831
ROCabral <rocabral.adm@gmail.com>
2024-10-25 00:03:00 -0300

    switch statement

--- 01_fundamentals_part_1/script.js
+++ 01_fundamentals_part_1/script.js
@@ -224,3 +224,28 @@ if (shouldDrive) {
 } else {
   console.log("Someone else should drive...");
 }
+
+//switch statement
+const day = "monday";
+switch (day) {
+  case "monday":
+    console.log("plan course structure");
+    console.log("Go to coding meetup");
+    break;
+  case "tuesday":
+    console.log("Prepare theory videos");
+    break;
+  case "wednesday":
+  case "thursday":
+    console.log("Write code examples");
+    break;
+  case "friday":
+    console.log("Record videos");
+    break;
+  case "saturday":
+  case "sunday":
+    console.log("Enjoy the weekend");
+    break;
+  default:
+    console.log("Not is a valid day!");
+}
