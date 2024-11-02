a6bedb418d9b67b15b6347950f06b90c5f0ff2b0
ROCabral <rocabral.adm@gmail.com>
2024-11-01 23:46:46 -0300

    Arrow function and functions calling functions.

--- 06_fundamentals_part_2/script.js
+++ 06_fundamentals_part_2/script.js
@@ -20,10 +20,20 @@ function logger() {
 //calling | running | invoking the function
 logger();
 
+//Functions calling other functions
+function cutFruitPieces(fruit) {
+  return fruit * 4;
+}
+
 function fruitProcessor(apples, oranges) {
-  const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+  const applePieces = cutFruitPieces(apples);
+  const orangePieces = cutFruitPieces(oranges);
+
+  const juice = `Juice with ${applePieces} pieces of apples and ${orangePieces} pieces of oranges`;
   return juice;
 }
+
+console.log(fruitProcessor(2, 3));
 
 //armazenando retorno e imprimindo
 const appleJuice = fruitProcessor(5, 0);
@@ -47,3 +57,18 @@ const calcAge2 = function (birthYear) {
 const age2 = calcAge1(1983);
 
 console.log(age1, age2);
+
+//Arrow Function
+const calcAge3 = (birthYear) => 2024 - birthYear;
+const age3 = calcAge3(1983);
+console.log(age3);
+
+const yearUntilRetirement = (birthYear, firstName) => {
+  const age = 2037 - birthYear;
+  const retirement = 65 - age;
+  //return retirement;
+  return `${firstName} retires in ${retirement} years`;
+};
+
+console.log(yearUntilRetirement(1983, "Rodrigo Cabral"));
+console.log(yearUntilRetirement(1979, "Patrícia Ally"));
