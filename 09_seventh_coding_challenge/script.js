3782f4b8bdab4de88e74c82ef2e62848dbdd36d2
ROCabral <rocabral.adm@gmail.com>
2024-11-09 20:25:40 -0300

    seventh codding challenge.

---
+++ 09_seventh_coding_challenge/script.js
@@ -0,0 +1,33 @@
+const mark = {
+  fullName: "Mark Miller",
+  mass: 78,
+  height: 1.69,
+
+  bmi: function calcBMI() {
+    return this.mass / (this.height * this.height);
+  },
+};
+
+const john = {
+  fullName: "John Smith",
+  mass: 92,
+  height: 1.95,
+
+  bmi: function calcBMI() {
+    return this.mass / (this.height * this.height);
+  },
+};
+
+if (mark.bmi > john.bmi) {
+  console.log(
+    `${mark.fullName} BMI ${mark.bmi()} is higher than ${
+      john.fullName
+    } BMI ${john.bmi()}`
+  );
+} else {
+  console.log(
+    `${john.fullName} BMI ${john.bmi()} is higher than ${
+      mark.fullName
+    } BMI ${mark.bmi()}`
+  );
+}
